901ac966422e135722e2649989b54c03de16ae64
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-08-02 13:28:44 -0400

    added api information to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
-console.log("test");
+//movie section global variables
 
+//fetch movie api information
+
+fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&language=en", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "52418e293amsh3c28249d07dbf86p11787ejsn25c24e4b2347",
+		"x-rapidapi-host": "streaming-availability.p.rapidapi.com"
+	}
+})
+.then(response => response.json())
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});

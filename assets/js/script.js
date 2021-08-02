10883ca2ce3e2bba4601b95a489fb30762434f40
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-08-02 13:56:23 -0400

    added if statement to script file to add genre id to the api call, changed html id classes back to genre names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 //movie section global variables
+var genreId = ""
 
 //fetch movie api information
-
-fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&language=en", {
+fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=" 18 "&page=1&language=en", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "52418e293amsh3c28249d07dbf86p11787ejsn25c24e4b2347",
@@ -16,3 +16,33 @@ fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&ser
 .catch(err => {
 	console.error(err);
 });
+
+//use checkbox data to insert genre into api search
+if ($('#action').is(':checked')) {
+    genreId = "28";
+}
+if ($('#adventure').is(':checked')) {
+    genreId = "12";
+}
+if ($('#comedy').is(':checked')) {
+    genreId = "35";
+}
+if ($('#horror').is(':checked')) {
+    genreId = "27";
+}
+if ($('#drama').is(':checked')) {
+    genreId = "18";
+}
+if ($('#romance').is(':checked')) {
+    genreId = "10749";
+}
+if ($('#scifi').is(':checked')) {
+    genreId = "878";
+}
+if ($('#family').is(':checked')) {
+    genreId = "10751";
+} else {
+    alert('You must select a movie Genre!');
+}
+
+

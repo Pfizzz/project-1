2bea20e371516ef1aacbf54f3ed5976ff3ab012f
Chasetopher <85038033+Chasetopher@users.noreply.github.com>
2021-08-02 20:40:16 -0400

    fixed bug where genreId was undefined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,71 @@
 //movie section global variables
-var genreId = ""
+var movieSearchEl = $('#generate-movie-btn');
+var genreId = 0;
 
-//fetch movie api information
-fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=" 18 "&page=1&language=en", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "52418e293amsh3c28249d07dbf86p11787ejsn25c24e4b2347",
-		"x-rapidapi-host": "streaming-availability.p.rapidapi.com"
-	}
-})
-.then(response => response.json())
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
+
+//choose movie genre button click
+$(document).ready(function() {
+    $("#generate-movie-btn").click(function(){
+        //run getGenre function to change the api url to fetch data based on the users choice.
+        getGenre(genreId);
+    }); 
 });
 
 //use checkbox data to insert genre into api search
-if ($('#action').is(':checked')) {
-    genreId = "28";
-}
-if ($('#adventure').is(':checked')) {
-    genreId = "12";
-}
-if ($('#comedy').is(':checked')) {
-    genreId = "35";
-}
-if ($('#horror').is(':checked')) {
-    genreId = "27";
-}
-if ($('#drama').is(':checked')) {
-    genreId = "18";
-}
-if ($('#romance').is(':checked')) {
-    genreId = "10749";
-}
-if ($('#scifi').is(':checked')) {
-    genreId = "878";
-}
-if ($('#family').is(':checked')) {
-    genreId = "10751";
-} else {
-    alert('You must select a movie Genre!');
+var getGenre = function(genreId) {
+    var id = genreId;
+    console.log(id);
+    //change the value of genreId based on which checkbox is clicked
+    if ($('#action').is(':checked')) {
+       id = 28;
+       getMovies(genreId);
+    }
+    if ($('#adventure').is(':checked')) {
+       id = 12;
+       getMovies(genreId);
+    }
+    if ($('#comedy').is(':checked')) {
+       id = 35;
+       getMovies(genreId);
+    }
+    if ($('#horror').is(':checked')) {
+       id = 27;
+       getMovies(genreId);
+    }
+    if ($('#drama').is(':checked')) {
+       id = 18;
+       getMovies(genreId);
+    }
+    if ($('#romance').is(':checked')) {
+       id = 10749;
+       getMovies(genreId);
+    }
+    if ($('#scifi').is(':checked')) {
+       id = 878;
+       getMovies(genreId);
+    }
+    if ($('#family').is(':checked')) {
+       id = 10751;
+       getMovies(genreId);
+    } 
 }
 
 
+var getMovies = function (genreId) {
+    //fetch movie api information
+    fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=" + genreId + "&page=1&language=en", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "52418e293amsh3c28249d07dbf86p11787ejsn25c24e4b2347",
+            "x-rapidapi-host": "streaming-availability.p.rapidapi.com"
+        }
+    })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            console.log(genreId);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}

3a79557d359b5c84140b2cb6a46604f64c3495ad
Justin Perez <perezjrp936@gmail.com>
2021-08-05 15:44:43 -0400

    consolidated important restaurant data and have it load on single.html

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,8 +1,14 @@
 var moviesList = [];
+var restaurantsList = [];
 
 var loadMovies = function(){
     moviesList = JSON.parse(localStorage.getItem("movieList"));
     console.log(moviesList);
+};
+
+var loadMovies = function(){
+    restaurantsList = JSON.parse(localStorage.getItem("restaurantList"));
+    console.log(restaurantsList);
 };
 
 loadMovies();
